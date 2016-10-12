d41556049164da8831f50a097a2ef9b9cb6eefee
Sujay Tadwalkar <sujayt123@gmail.com>
2016-10-12 13:22:46 -0500

    num cpus is now a param

--- seam_carving_distributed.py
+++ seam_carving_distributed.py
@@ -3,6 +3,7 @@ import cv2
 import math
 import numpy as np
 import sys
+import time
 
 def compute_scoring_matrix(color_image):  
     """
@@ -71,14 +72,13 @@ def process_main(num_rows, energy_matrix, start_idx, end_idx, left_edge_conn=Non
     # Notify the master process of the results. The master needs access to both the final row of the DP matrix, as well as the parent matrix.
     return (prev, parent)
 
-def detect_seam(energy_matrix, pool):
+def detect_seam(energy_matrix, pool, num_cpus):
     """
     Uses dynamic programming to detect a minimum cost contiguous seam from top to bottom of the input image.
 
     param:  energy_matrix    the cost matrix 
     return: path             a list of the coordinates of the pixels in the minimum cost seam
     """        
-    num_cpus = 3
     tasks = []
     chunk_length = (int) (math.ceil(1.0 * len(energy_matrix[0]) / num_cpus))        
     left_pipe_for_curr_process = None
@@ -123,7 +123,7 @@ def carve_seam(seam, color_image, energy_matrix):
         color_image[pixel[0]].pop(pixel[1])        
     return color_image
 
-def main(path_to_image, path_to_output):    
+def main(path_to_image, path_to_output, num_cpus):    
     """
     Executes the program.
 
@@ -131,12 +131,15 @@ def main(path_to_image, path_to_output):
     param:  path_to_output  the path to the desired output video
     """   
     color_image = cv2.imread(path_to_image)         
-    video = cv2.VideoWriter(path_to_output, cv2.VideoWriter_fourcc(*'MJPG'), 15, (len(color_image[0]), len(color_image)))   
-    pool = Pool(3)
+    video = cv2.VideoWriter(path_to_output, cv2.VideoWriter_fourcc(*'MJPG'), 15, (len(color_image[0]), len(color_image)))           
+    pool = Pool(num_cpus)
     # We can use a with statement to ensure threads are cleaned up promptly
     for i in range(len(color_image[0]) - 20):
         energy_matrix = compute_scoring_matrix(color_image)        
-        seam = detect_seam(energy_matrix, pool)            
+        start = time.clock()
+        seam = detect_seam(energy_matrix, pool, num_cpus)            
+        end = time.clock()
+        # print("It took", end-start, " to run through the seam method with ", num_cpus, "cpus.")
         color_image = np.asarray(carve_seam(seam, color_image.tolist(), energy_matrix), dtype=np.uint8)
         video.write(cv2.copyMakeBorder(color_image, 0, 0, 0, i + 1, cv2.BORDER_CONSTANT, value=[255,255,255]))
 
@@ -154,12 +157,12 @@ if __name__ == "__main__":
         print()
         print("Options:")
         print("-h".ljust(40), "shows this help message".ljust(50))
-        print("<input_filepath> <output_filepath>".ljust(40), "input_filepath  is the path (absolute or relative) to the input image".ljust(50))
+        print("<input_filepath> <output_filepath> <num_cpus>".ljust(40), "input_filepath  is the path (absolute or relative) to the input image".ljust(50))
         print(" " * 41 + "output_filepath is the path (absolute or relative) to the desired output video [must end with a .avi extension]".ljust(50))
         print() 
         sys.exit(0)
-    if len(args) <= 2 or args[2].endswith(".avi") is False:    
+    if len(args) < 4 or args[2].endswith(".avi") is False:
         print("Please specify an appropriate set of command-line arguments. Run with -h flag for more details.", file=sys.stderr)        
         sys.exit(1)
         
-    main(args[1], args[2])
+    main(args[1], args[2], int(args[3]))
